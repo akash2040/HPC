591ec312185be673acaa9e926f2f45fe59aea8ca
akash2040 <76614970+akash2040@users.noreply.github.com>
2020-12-26 16:55:07 +0545

    Add files via upload

---
+++ 6cs005_PortfolioS1_19_20_2040368_Akash_Chanara/POSIX/c/5/2040368_Task2_C_5.c
@@ -0,0 +1,132 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <crypt.h>
+#include <time.h>
+#include <pthread.h>
+
+/***********************************************************************
+*******
+
+  Compile with:
+    cc -o task2c5 2040368_Task2_C_5.c -lcrypt -lrt -pthread
+    
+	./task2c5 > task2c5.txt
+************************************************************************
+******/
+
+
+int n_passwords = 1;
+
+char *encrypted_passwords[] = {
+
+"$6$AS$m5exzg8cZG1i2uh.7ohQlRxbutF91rihO63V.kpCF8WkS185/RSz1k/fEgmukhb1XKRyL7hqDR.q1.1GHfj2x0"
+};
+
+
+
+void substr(char *dest, char *src, int start, int length){
+  memcpy(dest, src + start, length);
+  *(dest + length) = '\0';
+}
+
+
+void run()
+{
+  int i;
+pthread_t thread_1, thread_2;
+
+    void *kernel_function_1();
+    void *kernel_function_2();
+for(i=0;i<n_passwords;i<i++) {
+   
+    
+    pthread_create(&thread_1, NULL,kernel_function_1, encrypted_passwords[i]);
+    pthread_create(&thread_2, NULL,kernel_function_2, encrypted_passwords[i]);
+
+    pthread_join(thread_1, NULL);
+    pthread_join(thread_2, NULL);
+ }
+}
+
+void *kernel_function_1(void *salt_and_encrypted){
+  int a, k, s;     // Loop counters
+  char salt[7];    // String used in hahttps://www.youtube.com/watch?v=L8yJjIGleMwshing the password. Need space
+  char plain[7];   // The combination of letters currently being checked
+  char *enc;       // Pointer to the encrypted password
+  int count = 0;   // The number of combinations explored so far
+
+  substr(salt, salt_and_encrypted, 0, 6);
+
+  for(a='A'; a<='M'; a++){
+    for(k='A'; k<='Z'; k++){
+      for(s=0; s<=99; s++){
+        sprintf(plain, "%c%c%02d", a,k,s);
+        enc = (char *) crypt(plain, salt);
+        count++;
+        if(strcmp(salt_and_encrypted, enc) == 0){
+          printf("#%-8d%s %s\n", count, plain, enc);
+        } 
+      }
+    }
+  }
+  printf("%d solutions explored\n", count);
+}
+
+
+void *kernel_function_2(void *salt_and_encrypted){
+  int a, k, s;     // Loop counters
+  char salt[7];    // String used in hahttps://www.youtube.com/watch?v=L8yJjIGleMwshing the password. Need space
+  char plain[7];   // The combination of letters currently being checked
+  char *enc;       // Pointer to the encrypted password
+  int count = 0;   // The number of combinations explored so far
+
+  substr(salt, salt_and_encrypted, 0, 6);
+
+  for(a='N'; a<='Z'; a++){
+    for(k='A'; k<='Z'; k++){
+      for(s=0; s<=99; s++){
+        sprintf(plain, "%c%c%02d", a,k,s);
+        enc = (char *) crypt(plain, salt);
+        count++;
+        if(strcmp(salt_and_encrypted, enc) == 0){
+          printf("#%-8d%s %s\n", count, plain, enc);
+        }
+      }
+    }
+  }
+  printf("%d solutions explored\n", count);
+}
+
+//Calculating time
+
+int time_difference(struct timespec *start, struct timespec *finish, long long int *difference)
+ {
+	  long long int ds =  finish->tv_sec - start->tv_sec; 
+	  long long int dn =  finish->tv_nsec - start->tv_nsec; 
+
+	  if(dn < 0 ) {
+	    ds--;
+	    dn += 1000000000; 
+  } 
+	  *difference = ds * 1000000000 + dn;
+	  return !(*difference > 0);
+}
+int main(int argc, char *argv[])
+{
+  	
+	struct timespec start, finish;   
+  	long long int time_elapsed;
+
+  	clock_gettime(CLOCK_MONOTONIC, &start);
+
+  	
+	
+    		run();
+  	
+	clock_gettime(CLOCK_MONOTONIC, &finish);
+	  time_difference(&start, &finish, &time_elapsed);
+	  printf("Time elapsed was %lldns or %0.9lfs\n", time_elapsed,
+		                                 (time_elapsed/1.0e9)); 
+  return 0;
+}
